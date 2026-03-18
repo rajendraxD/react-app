dfb68a4031e9722a5371ecb32b444dd6e5270155
rajendraxD <rajendraxd1@gmail.com>
2026-03-18 19:07:41 +0530

    feat: Initialize new React project with Vite, ESLint, and Tailwind CSS.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-4xl font-bold">App</h1>
+      </div>
+    </div>
+  );
+}
