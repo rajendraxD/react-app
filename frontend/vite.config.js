3de45208068ff0217e8fe42f249ebc4ef68d05cc
rajendraxD <rajendraxd1@gmail.com>
2026-04-06 14:57:56 +0530

    feat: initialize frontend project with login page, Tailwind CSS, and Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import compression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+    compression(),
   ],
-})
+});
